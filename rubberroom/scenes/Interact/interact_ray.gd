1401921379b9f7ebcdf08f11a5e353819e0260cb
OneEgg1Dank <ryan.gotanewemail@gmail.com>
2024-11-04 12:12:58 -0500

    Interaction 1
    Buttons, Interact Ray, Basic Crosshair, MANGO MANGO MANGO

---
+++ rubberroom/scenes/Interact/interact_ray.gd
@@ -0,0 +1,13 @@
+extends RayCast3D
+@onready var prompt: Label = $Prompt
+
+
+func _physics_process(delta: float) -> void:
+	prompt.text = ""
+	
+	
+	if is_colliding():
+		var collider = get_collider()
+		
+		if collider is Interactable:
+			prompt.text = collider.prompt_msg
