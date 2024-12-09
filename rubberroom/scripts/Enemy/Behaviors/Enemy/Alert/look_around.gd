4ba58eacdd16bbea94f8799a8fd3983bb3c0517d
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2024-12-09 15:04:28 -0500

    Added TODO comments

--- rubberroom/scripts/Enemy/Behaviors/Enemy/Alert/look_around.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Alert/look_around.gd
@@ -1,5 +1,6 @@
 extends ActionLeaf
 
+# TODO: rotate_to_look() function runs too quickly, needs to check if its at the rotation before leaving this actionleaf
 
 func tick(actor: Node, blackboard: Blackboard) -> int:
 	actor.rotate_to_look()
--- rubberroom/scripts/Enemy/Behaviors/Enemy/Hostile/set_destination_to_enemy.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Hostile/set_destination_to_enemy.gd
@@ -1,5 +1,10 @@
 extends ActionLeaf
 
+# TODO: Research and confirm the issues:
+# How to get it to run like the patrol state so that it continues until it is in range to attack.
+# How to traverse the tree properly so it does not return here if you are not detected
+# How to keep the enemy in the hostile state while its running
+
 # Set target to player, look at player
 func tick(actor, _blackboard):
 	actor.update_target_location(actor.target.global_position)
