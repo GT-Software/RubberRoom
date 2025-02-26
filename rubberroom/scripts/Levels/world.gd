b377c144a91fd55b15fcbff89c7e8a0b38c35a7c
Brandon Grunes <63303557+bgrunes@users.noreply.github.com>
2025-02-26 11:10:53 -0500

    Added spanw player function in world.gd

--- rubberroom/scripts/Levels/world.gd
+++ rubberroom/scripts/Levels/world.gd
@@ -3,17 +3,26 @@ extends Node3D
 @onready var player = %Player
 @onready var change_room_button: Interactable = $"NavigationRegion3D/Base World/Interactables/Change Room Button"
 @onready var load_scene_button: Interactable = $"NavigationRegion3D/Base World/Interactables/Load Scene Button"
+@onready var spawnpoint = get_tree().find_child('Spawn Point Debug Marker')
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	change_room_button.connect("change_room", spawn_room)
 	load_scene_button.connect("load_scene", load_new_level)
+	spawn_player_controller()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+
+func spawn_player_controller():
+	var player_scene = load("res://scenes/Player/player.tscn")
+	var instance = player_scene.instantiate()
+	get_tree().current_scene.add_child(instance)
+	player_scene.global_position = spawnpoint.global_position
+	player_scene.global_position.y += 1
 
 func spawn_room():
 	var new_room_scene = RoomManager.get_random_room(false)  # Pick a room and remove it from the sack
@@ -22,8 +31,7 @@ func spawn_room():
 		add_child(new_room)  # Add it to the scene tree
 		new_room.global_transform.origin = Vector3(110, 0, 0)  # Adjust position if needed
 		
-		# Get Spawnpoint debug marker, place player at marker
-		var spawnpoint = new_room.find_child('Spawn Point Debug Marker')
+		# Place player at Spawnpoint marker
 		player.global_position = spawnpoint.global_position
 		player.global_position.y += 1
 		print('Room generated!')
