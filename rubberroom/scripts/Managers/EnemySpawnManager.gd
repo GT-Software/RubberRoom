04163a68f943e6c3194befc0630b1a7079465508
Brandon Grunes <bgrunes14@gmail.com>
2025-03-29 14:59:11 -0400

    #18 Added Enemy Sack Setup
    Included a resource file and script for resource file, Singleton for Enemy Spawning.

---
+++ rubberroom/scripts/Managers/EnemySpawnManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var sack_res = preload('res://data/enemy_sack.tres')
+
+# threat_level will tell the system what enemy classes should be spawning.
+# higher threat level means harder/more complex enemies spawn in a room randomly.
+var threat_level = 0
+
+# Spawn random enemies based on number of markers and spawn percentage chance
+func spawn_enemies(markers : Array):
+	pass
+
+# Choose an enemy to spawn
+func choose_enemy() -> PackedScene:
+	pass
