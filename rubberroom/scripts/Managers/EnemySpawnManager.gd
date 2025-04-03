309eb65d5722553cc1e623c3f2ed8e55407f4819
Brandon Grunes <bgrunes14@gmail.com>
2025-04-03 16:52:45 -0400

    Started spawn_enemies function
    Added random marker selection algorithm O(n) complexity. Need to see if can optimize

--- rubberroom/scripts/Managers/EnemySpawnManager.gd
+++ rubberroom/scripts/Managers/EnemySpawnManager.gd
@@ -7,8 +7,33 @@ var sack_res = preload('res://data/enemy_sack.tres')
 var threat_level = 0
 
 # Spawn random enemies based on number of markers and spawn percentage chance
-func spawn_enemies(markers : Array, level : int):
+func spawn_enemies(markers : Array, level : int = 0):
+	var current_sack : Array
 	
+	# Depending on the progression level, spawn these types of enemies
+	match level:
+		0:
+			current_sack.push_back(sack_res.sack["basic enemy"])
+		1:
+			current_sack.push_back(sack_res.sack["basic enemy"])
+			current_sack.push_back(sack_res.sack["ranged enemy"])
+		2: 
+			current_sack.push_back(sack_res.sack["basic enemy"])
+			current_sack.push_back(sack_res.sack["ranged enemy"])
+			current_sack.push_back(sack_res.sack["heavy enemy"])
+	
+	# STOP. HAMMER TIME.
+	
+	var numEnemies = ceil((markers.size() + current_sack.size()) / 2)
+	var markersToSpawn : Array
+	for marker in markers:
+		var randNum = randf_range(0.0, 100.0)
+		
+		# if randNum is 50.0 or higher, choose this marker
+		if randNum >= 50.0:
+			markersToSpawn.push_back(marker)
+	
+	return null
 
 # Choose an enemy to spawn
 func choose_enemy() -> PackedScene:
