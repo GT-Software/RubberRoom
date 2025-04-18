a1c9635d8b515f8f2f2e9d4ec800ee8ae8d49cfa
Brandon Grunes <bgrunes14@gmail.com>
2025-04-18 14:11:38 -0400

    Added Basic Enemy Beehave Tree

--- rubberroom/scenes/Behaviors/face_direction.gd
+++
@@ -1,8 +0,0 @@
-extends ActionLeaf
-
-# Rotate the actor towards the direction of the next path position of the navigation agent
-func tick(actor: Node, blackboard: Blackboard) -> int:
-	actor.rotate_self = true
-	actor.rotate_target = actor.nav_agent.get_next_path_position()
-	actor.rotate_node = actor.target
-	return SUCCESS
