e0709909b9a49f93374375fe90921c17e46acaa9
Brandon Grunes <bgrunes14@gmail.com>
2025-04-19 12:38:18 -0400

    Update idle_animation.gd
    Commented out original code, started using the enemy.gd functions and variables to update the animation so it works

--- rubberroom/scripts/Enemy/Behaviors/Enemy/Idle/idle_animation.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Idle/idle_animation.gd
@@ -4,12 +4,14 @@ extends ActionLeaf
 func tick(actor, blackboard: Blackboard):
 	# Ryan: Put your animation calls here. The idea is that it would pick 
 	# a random idle animation when we make new animations.
-	var ap = actor.anim_tree.get_node(actor.anim_tree.anim_player)
+	#var ap = actor.anim_tree.get_node(actor.anim_tree.anim_player)
 	# Initial idle animation
-	if ap.current_animation == "":
-		ap.play("Idle(1)0")
-	
-	if ap.is_playing():
-		ap.animation_set_next("Idle(1)0", "Idle(2)0")
+	#if ap.current_animation == "":
+		#ap.play("Idle(1)0")
+	#
+	#if ap.is_playing():
+		#ap.animation_set_next("Idle(1)0", "Idle(2)0")
+	actor.is_idle = true
+	actor.update_animation_state()
 	
 	return SUCCESS
