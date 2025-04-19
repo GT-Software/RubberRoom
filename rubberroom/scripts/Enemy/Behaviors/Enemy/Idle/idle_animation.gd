3d2c741e0e28f2dfe7a8dd3f1143852c77dcaf2d
Brandon Grunes <bgrunes14@gmail.com>
2025-04-19 10:38:19 -0400

    fixed bug, game playable again

--- rubberroom/scripts/Enemy/Behaviors/Enemy/Idle/idle_animation.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Idle/idle_animation.gd
@@ -4,12 +4,12 @@ extends ActionLeaf
 func tick(actor, blackboard: Blackboard):
 	# Ryan: Put your animation calls here. The idea is that it would pick 
 	# a random idle animation when we make new animations.
-	
+	var ap = actor.anim_tree.get_node(actor.anim_tree.anim_player)
 	# Initial idle animation
-	if actor.anim_tree.anim_player.current_animation == "":
-		actor.anim_tree.anim_player.play("Idle(1)0")
+	if ap.current_animation == "":
+		ap.play("Idle(1)0")
 	
-	if actor.anim_tree.anim_player.is_playing():
-		actor.anim_tree.anim_player.animation_set_next("Idle(1)0", "Idle(2)0")
+	if ap.is_playing():
+		ap.animation_set_next("Idle(1)0", "Idle(2)0")
 	
 	return SUCCESS
