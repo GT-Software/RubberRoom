5f01171d646005e60a946e98b44438e97c5ccb6e
Brandon Grunes <bgrunes14@gmail.com>
2025-04-28 15:10:31 -0400

    Version 4.4.1 Upgrade

--- rubberroom/scripts/Interact/actionable.gd
+++ rubberroom/scripts/Interact/actionable.gd
@@ -1,7 +1,7 @@
 # Actionable.gd
 extends Interactable
 
-@export var dialogue_resource: DialogueResource
+#@export var dialogue_resource: DialogueResource
 @export var dialogue_start: String = "start"
 
 # Add a flag to prevent multiple activations
@@ -18,15 +18,15 @@ func action() -> void:
 	#only if within a certian radius of the actionable
 	print("Action method called")
 	# Connect to the DialogueManager signals
-	DialogueManager.connect("got_dialogue", _on_got_dialogue)
-	DialogueManager.connect("dialogue_ended", _on_dialogue_finished)
-	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
+	#DialogueManager.connect("got_dialogue", _on_got_dialogue)
+	#DialogueManager.connect("dialogue_ended", _on_dialogue_finished)
+	#DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
 
 func _on_got_dialogue(line):
 	# This will be called when the first line of dialogue is encountered
 	_on_dialogue_started()
 	# Disconnect to prevent this from being called multiple times
-	DialogueManager.disconnect("got_dialogue", _on_got_dialogue)
+	#DialogueManager.disconnect("got_dialogue", _on_got_dialogue)
 
 func _on_dialogue_started():
 	# Pause the game
@@ -38,4 +38,4 @@ func _on_dialogue_started():
 func _on_dialogue_finished(resource):
 	# Re-enable interaction when the dialogue ends
 	is_active = false
-	DialogueManager.disconnect("dialogue_ended", _on_dialogue_finished)
+	#DialogueManager.disconnect("dialogue_ended", _on_dialogue_finished)
