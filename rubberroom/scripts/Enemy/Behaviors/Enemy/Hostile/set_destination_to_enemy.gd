d8a2765fd4da518e71556164242fe01fd617dd9c
Brandon Grunes <bgrunes14@gmail.com>
2025-04-30 18:43:35 -0400

    Fixed issue with animations not playing

--- rubberroom/scripts/Enemy/Behaviors/Enemy/Hostile/set_destination_to_enemy.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Hostile/set_destination_to_enemy.gd
@@ -8,10 +8,7 @@ extends ActionLeaf
 # Set target to player, look at player
 func tick(actor, blackboard):
 	print("Enemy: Chasing Player")
-	if blackboard.get_value("can_see_player") == true:
-		actor.update_target_location(blackboard.get_value("player_pos"))
-	else:
-		return FAILURE
+	actor.update_target_location(blackboard.get_value("player_pos"))
 		
 	#actor.update_nav_agent()
 	return SUCCESS
--- rubberroom/scripts/Enemy/Behaviors/Enemy/Patrol/await_arrival_chase.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/Patrol/await_arrival_chase.gd
@@ -5,14 +5,8 @@ func tick(actor, blackboard):
 	if actor.is_in_range or actor.nav_agent.is_target_reached():
 		#blackboard.set_value('attacking', true)
 		return FAILURE
-	elif actor.target == null:
-		#blackboard.set_value('attacking', false)
-		return FAILURE
-	elif actor.nav_agent.is_target_reached():
-		#actor.update_nav_agent(actor.RUN_SPEED)
-		return RUNNING
 	else:
-		actor.update_target_location(actor.target.position)
+		actor.update_target_location(blackboard.get_value("player_pos"))
 		#actor.update_nav_agent(actor.RUN_SPEED)
 		actor.is_idle = false
 		actor.is_walking = false
