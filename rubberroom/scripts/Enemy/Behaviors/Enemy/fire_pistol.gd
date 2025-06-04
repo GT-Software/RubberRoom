605d6b98a7e6290153fa9ab3e7c34118f20d0f38
bgrunes <bgrunes14@gmail.com>
2025-06-04 12:45:03 -0400

    Added firing code to fire_pistol and fire_rifle

--- rubberroom/scripts/Enemy/Behaviors/Enemy/fire_pistol.gd
+++ rubberroom/scripts/Enemy/Behaviors/Enemy/fire_pistol.gd
@@ -3,22 +3,45 @@ extends ActionLeaf
 
 func tick(actor, blackboard: Blackboard):
 	var weapon = actor.current_weapon
+	var weapon_model = actor.current_weapon_model
+	var muzzle_position = weapon_model.get_node("MuzzlePosition")
+	
+	# Query a ray from the weapaon to the player
+	var ray_query = PhysicsRayQueryParameters3D.create(muzzle_position.global_position, blackboard.get_value("player_pos"))
+	var result = actor.get_world_3d().direct_space_state.intersect_ray(ray_query)
+	
+	# Grab where to aim
+	var aim_point
+	if result:
+		aim_point = result.position
+	else:
+		return SUCCESS
+	
+	
+	# Calculate direction from muzzle to aim point
+	var base_direction
+	if muzzle_position:
+		base_direction = (aim_point - muzzle_position.global_position).normalized()
+	else:
+		base_direction = (aim_point - actor.weapon_attachment.global_position).normalized()
+	
 	# Spawn projectile(s)
 	var projectile_count = weapon.projectile_count
 	for i in range(projectile_count):
 		# Create projectile instance
 		var projectile = weapon.projectile_scene.instantiate()
+		projectile.set_collision_mask_value(3, false)
 		actor.projectile_container.add_child(projectile)
 		
 	# Set projectile position to muzzle or fallback to weapon attachment
-		if actor.muzzle_position:
-			projectile.global_position = actor.muzzle_position.global_position
+		if muzzle_position:
+			projectile.global_position = muzzle_position.global_position
 		else:
 			print("Warning: MuzzlePosition is null, using weapon attachment position!")
 			projectile.global_position = actor.weapon_attachment.global_position
 		
 		# Calculate projectile direction with spread
-		var direction = actor.base_direction
+		var direction = base_direction
 		
 		# Add spread if applicable
 		if weapon.spread > 0.0 or i > 0:
@@ -31,19 +54,22 @@ func tick(actor, blackboard: Blackboard):
 		projectile.setup(
 			weapon.damage,
 			weapon.projectile_speed,
-			actor.direction,
+			blackboard.get_value("player_pos"),
 			actor.global_position,
 			actor
 		)
+		
+		if actor.firing_sound:
+			actor.firing_sound.stream = weapon.sound_fire
+			actor.firing_sound.play()
 		
 		# Optional: Spawn muzzle flash effect
 		if weapon.muzzle_flash_scene:
 			var muzzle_flash = weapon.muzzle_flash_scene.instantiate()
 			if actor.muzzle_position:
-				actor.muzzle_position.add_child(muzzle_flash)
+				weapon.muzzle_position.add_child(muzzle_flash)
 			else:
 				actor.weapon_attachment.add_child(muzzle_flash)
 		
-	
-	print("Fired Pistol")
+	print("Fired Shotgun")
 	return FAILURE
